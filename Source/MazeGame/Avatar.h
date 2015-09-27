96aa2c7b23669da7f8a58f90ffed5df68b4a65b0
chocokitty24 <foglek13@highpoint.edu>
2015-09-27 17:39:55 -0400

    Quick edit test for will

--- Source/MazeGame/Avatar.h
+++ Source/MazeGame/Avatar.h
@@ -23,6 +23,7 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
+	//Hello Will says this comment
 	
 	
 };
